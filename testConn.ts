29bc6e16acc0c963ab2a0c4bbed746863a3be27d
RUSHI-0007 <rushipk16@gmail.com>
2026-08-10 15:28:08 +0530

    Remove temp scripts

--- testConn.ts
+++
@@ -1,6 +0,0 @@
-import { Pool } from "pg";
-const connectionString = "postgresql://postgres.ifrismvjfuoaqjyfhgdh:Rushik%241600%23@aws-0-ap-south-1.pooler.supabase.com:5432/postgres";
-const pool = new Pool({ connectionString, ssl: { rejectUnauthorized: false } });
-pool.query("SELECT 1")
-  .then(() => { console.log("Connected on 5432!"); process.exit(0); })
-  .catch((e) => { console.error("Error on 5432:", e); process.exit(1); });
